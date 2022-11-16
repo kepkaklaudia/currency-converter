b84d33d18a1fef7a10c28979ca3ca3894f11da35
Klaudia Kepka <kepkaklaudia@gmail.com>
2022-11-16 13:06:38 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,32 @@
+# Currency Converter Website
+![gif](images/Currency Converter.gif)
+Made by Klaudia Kępka
+
+## Currency Converter Demo Website:
+https://kepkaklaudia.github.io/currencyConverter/
+
+## Quick Preview
+![gifPreview](images/preview.gif)
+
+## First field
+In the first field, you can enter the amount in PLN that you want to convert to another currency. You can enter the amount using the keyboard or use the arrows hidden in the right part of the field. The accuracy is 0.01.
+
+## Second field
+Here you can choose the currency to which you want to convert your PLN amount. You can choose EUR, USD, GBP or CHF.
+
+## "Przelicz kwotę!" button
+There is a special button below the fields. When you click it, the converter can do the calculations.
+
+## Results
+When you click "Przelicz kwotę!" button, you will see your amount in the new currency after the words "Nowa kwota wynosi:"
+
+## Responsive version
+When you are in mobile mode (maximum width of screen: 600px) you can notice some changes. 
+The box is slightly smaller and the fields are arranged in one column to fit on mobile screens.
+
+## Technologies used
+- HTML
+- CSS
+- JavaScript, ES6+ Features
+- BEM Convention
+- normalise.css
---
+++ js/script.js
@@ -0,0 +1,42 @@
+{
+    const calculateResult = (quantity, rate) => {
+        const EUR = 4.70;
+        const USD = 4.51;
+        const GBP = 5.37;
+        const CHF = 4.80;
+
+        switch (rate) {
+            case "EUR":
+                return quantity / EUR;
+            case "USD":
+                return quantity / USD;
+            case "GBP":
+                return quantity / GBP;
+            case "CHF":
+                return quantity / CHF;
+        }
+    }
+
+    const updateResultText = (amount, rate) => {
+        const amountElement = document.querySelector(".js-amount");
+        amountElement.innerText = `${amount.toFixed(2)} ${rate}`;
+    }
+
+    const onFormSubmit = (event) => {
+        event.preventDefault();
+
+        const quantityElement = document.querySelector(".js-quantity");
+        const rateElement = document.querySelector(".js-rate");
+        const rate = rateElement.value;
+
+        let amount = calculateResult(+quantityElement.value, rate);
+        updateResultText(amount, rate);
+    }
+
+    const init = () => {
+        const formElement = document.querySelector(".js-form");
+        formElement.addEventListener("submit", onFormSubmit);
+    }
+
+    init();
+}
